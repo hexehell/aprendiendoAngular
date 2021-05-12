63570ca6957be0602df3a68513f0bbfc2ca39227
hexehell <mr.diaz@live.com>
2021-05-12 03:36:05 -0600

    00 como manipular un componente (primer commit)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
 
+
+//componente default
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html',//se ignora el templateurl si no esta1
+  styleUrls: ['./app.component.css'],
+  template: `<h1>tu mama</h1>`,//si esta el templateurl se ignora el template 
 })
 export class AppComponent {
-  title = 'bases';
+  public titulo = 'Contador';
+  public numero:number = 10;
+  incremento:number =2;
+
+  fnAcumular(pnValor:number)
+  {
+    this.numero += pnValor;
+
+  }
+
 }
